b94b49c69bfb5103c7ffc4e6caecea0c688ea9a4
Biswajit Singha <biswasingha.09@gmail.com>
2018-01-19 12:04:43 +0530

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,6 +1,19 @@
 const {SHA256}=require('crypto-js');
 const jwt=require('jsonwebtoken');
+const bcrypt=require('bcryptjs');
 
+var password='123abc!';
+/*bcrypt.genSalt(10,(err,salt)=>{
+	bcrypt.hash(password,salt,(err,hash)=>{
+		console.log(hash);
+	});
+});*/
+
+var hashPassword='$2a$10$4fBnKBIaft80AIlltmIdIeJ2AygfNEK7pMF.4sXStqu0q57T7YHF6';
+bcrypt.compare(password,hashPassword,(err,res)=>{
+	console.log(res);
+});
+/*
 var data={
 	id:10
 };
@@ -8,6 +21,7 @@ var token=jwt.sign(data,'bisu123');
 console.log(token);
 var decoded=jwt.verify(token,'bisu123');
 console.log('Decoded',decoded);
+*/
 
 /*
 var message='I am user number 3';
